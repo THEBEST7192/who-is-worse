1b25e4126d164d01460a119b91f76e8c10b636df
THEBEST7192 <blundsbakken@gmail.com>
2025-07-21 23:10:30 +0200

    asjdakdjwakdkaskajkdjsakdjksajkjdsa

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const COOKIE_EXPIRY_DAYS = 60;
-export const IMAGE_LOADING_SPIN_DELAY_MS = 500
+export const IMAGE_LOADING_SPIN_DELAY_MS = 500;
 export const DIFFICULTY_MEDIUM = 'medium';
 export const CATEGORY_ALL = 'all';
 
