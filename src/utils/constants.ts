5b7f895ff620aaa180c564a18f73e0b5d93a47f2
THEBEST7192 <blundsbakken@gmail.com>
2025-07-21 23:14:11 +0200

    E

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const COOKIE_EXPIRY_DAYS = 60;
-export const IMAGE_LOADING_SPIN_DELAY_MS = 500;
+export const IMAGE_LOADING_SPIN_DELAY_MS = 1000;
 export const DIFFICULTY_MEDIUM = 'medium';
 export const CATEGORY_ALL = 'all';
 
